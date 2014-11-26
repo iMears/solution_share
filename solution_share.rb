b39700864c09e15c661b773dced0dc790713620b
Maxwell Mears <maxwellmears@gmail.com>
2014-11-26 12:41:27 -0800

    tweaked formatting

--- solution_share.rb
+++ solution_share.rb
@@ -1,31 +1,24 @@
 require './dbc_data'
 
-
+puts "\nWelcome to Soution Share!\n"
 puts "Week number:"
 print ">"
 week_number = gets.chomp.to_i
 
-puts # empty line
-puts "Challenge number:"
+puts "\nChallenge number:"
 print ">"
 challenge_num = gets.chomp.to_i
 
-puts # empty line
-
-puts "1. Desert Rabbits"
-puts "2. Eastern Moose"
-puts "Choose Cohort name(by number):"
+puts "\n1. Desert Rabbits\n2. Eastern Moose"
+puts "Choose Cohort number:"
 print ">"
 cohort_num = gets.chomp.to_i
 case cohort_num
-  when 1
-    @cohort = @desert_rabbits
-  when 2
-    @cohort = @eastern_moose
-  end
-
-
-
+when 1
+  @cohort = @desert_rabbits
+when 2
+  @cohort = @eastern_moose
+end
 
 puts # empty line
 puts @cohort.sort_by.map{|k,v| v[0]} # prints sorted keys
@@ -39,25 +32,25 @@ while !@cohort.include?(student) # loop until given a valid last name
   student = gets.chomp.downcase.to_sym
 end
 
-puts # empty line
-puts "#{@cohort[student][0]}'s Week-#{week_number} Challenge-#{challenge_num} link:"
+puts "\n#{@cohort[student][0]}'s Week-#{week_number} Challenge-#{challenge_num} link:"
 
 final_url = "http://github.com/#{@cohort[student][1]}#{@all_weeks[week_number-1][challenge_num-1]}"
 puts final_url
-puts 'Would you like to visit them now?'
 
+puts 'Open link now?'
+print '>'
 visit = gets.chomp[0].downcase
-  if visit == 'y'
-    puts 'Opening in your browser'
-    exec('open ' + final_url)
-  else
-    puts 'Copy and paste the above link in your browser.'
-  end
+if visit == 'y'
+  puts 'Opening link...'
+  exec('open ' + final_url)
+else
+  puts 'Copy and paste link into browser.'
+end
 
-puts 'Would you like to review any other code?'
+puts 'Review more solutions?'
 again = gets.chomp[0].downcase
 if again == 'y'
-    exec('ruby solution_share.rb')
-  else
-    puts 'Have a nice day!'
-  end
+  exec('ruby solution_share.rb')
+else
+  puts 'Thanks for using Solution Share!'
+end
