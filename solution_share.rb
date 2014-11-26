a7f56143edca9f25214f753e190d33e297a5c9da
Maxwell Mears <maxwellmears@gmail.com>
2014-11-26 02:14:36 -0800

    moved data into dbc_data.rb and required it

--- solution_share.rb
+++ solution_share.rb
@@ -1,146 +1,4 @@
-
-week_1 = ['/phase-0-unit-1/tree/master/week-1/1-command-line',
-          '/phase-0-unit-1/tree/master/week-1/2-computer-setup',
-          '/phase-0-unit-1/tree/master/week-1/3-think-about-time',
-          '/phase-0-unit-1/tree/master/week-1/4-github-intro',
-          '/phase-0-unit-1/tree/master/week-1/5-fork-clone',
-          '/phase-0-unit-1/tree/master/week-1/6-tracking-changes',
-          '/phase-0-unit-1/tree/master/week-1/7-set-remotes',
-          '/phase-0-unit-1/tree/master/week-1/8-new-repo',
-          '/phase-0-unit-1/blob/master/week-1/9-twitter-intro.md',
-          '/phase-0-unit-1/blob/master/week-1/10-technical-blog.md',
-          '/phase-0-unit-1/blob/master/week-1/11-cultural-blog.md',
-          '/phase-0-unit-1/blob/master/week-1/12-self-introduction.md'
-        ]
-
-week_2 = ['/phase-0-unit-1/blob/master/week-2/1-cultural-assignment.md',
-          '/phase-0-unit-1/tree/master/week-2/2-site-planning',
-            '/phase-0-unit-1/tree/master/week-2/3-wireframing',
-            '/phase-0-unit-1/tree/master/week-2/4-beginning-html',
-            '/phase-0-unit-1/tree/master/week-2/5-beginning-css',
-            '/phase-0-unit-1/tree/master/week-2/6-blog-template',
-            '/phase-0-unit-1/tree/master/week-2/7-chrome-devtools',
-            '/phase-0-unit-1/tree/master/week-2/8-your-website-solo-challenge',
-            '/phase-0-unit-1/tree/master/week-2/9-testing-and-maintenance',
-            '/phase-0-unit-1/blob/master/week-2/10-technical-blog.md',
-            '/phase-0-unit-1/tree/master/week-2/11-gps1-1',
-            '/phase-0-unit-1/tree/master/week-2/12-BONUS-challenges'
-          ]
-
-week_3 = ['/phase-0-unit-1/tree/master/week-3/1-numbers-letters',
-          '/phase-0-unit-1/tree/master/week-3/2-methods',
-          '/phase-0-unit-1/tree/master/week-3/3-flow-control',
-          '/phase-0-unit-1/tree/master/week-3/4-arrays',
-          '/phase-0-unit-1/tree/master/week-3/5-more-methods',
-          '/phase-0-unit-1/tree/master/week-3/6-fix-the-errors',
-          '/phase-0-unit-1/blob/master/week-3/7-technical-blog.md',
-          '/phase-0-unit-1/blob/master/week-3/8-cultural-blog.md',
-          '/phase-0-unit-1/tree/master/week-3/9-gps1-2',
-          '/phase-0-unit-1/tree/master/week-3/10-BONUS-challenges'
-        ]
-
-week_4 = ['/phase-0-unit-2/tree/master/week-4/1-add-it-up',
-          '/phase-0-unit-2/tree/master/week-4/2-create-acct-groups',
-          '/phase-0-unit-2/tree/master/week-4/3-calculate-mode',
-          '/phase-0-unit-2/tree/master/week-4/4-group-research-methods',
-          '/phase-0-unit-2/tree/master/week-4/5-nums-commas-solo-challenge',
-          '/phase-0-unit-2/tree/master/week-4/6-pad-array',
-          '/phase-0-unit-2/tree/master/week-4/7-die-class',
-          '/phase-0-unit-2/blob/master/week-4/8-technical-blog.md',
-          '/phase-0-unit-2/blob/master/week-4/9-cultural-blog.md',
-          '/phase-0-unit-2/blob/master/week-4/10-accountability-group.md',
-          '/phase-0-unit-2/tree/master/week-4/11-BONUS-challenges'
-        ]
-
-week_5 = ['/phase-0-unit-2/tree/master/week-5/1-die',
-          '/phase-0-unit-2/tree/master/week-5/2-guessing-game',
-          '/phase-0-unit-2/tree/master/week-5/3-attr',
-          '/phase-0-unit-2/tree/master/week-5/4-cipher-challenge',
-          '/phase-0-unit-2/tree/master/week-5/5-bingo-solo-challenge',
-          '/phase-0-unit-2/tree/master/week-5/6-validate-credit-card',
-          '/phase-0-unit-2/blob/master/week-5/7-technical-blog.md',
-          '/phase-0-unit-2/blob/master/week-5/8-cultural-blog.md',
-          '/phase-0-unit-2/tree/master/week-5/9-gps2-1',
-          '/phase-0-unit-2/tree/master/week-5/10-BONUS-challenges'
-        ]
-
-week_6 = ['/phase-0-unit-2/tree/master/week-6/1-drawer-debugger',
-          '/phase-0-unit-2/tree/master/week-6/2-variable-scope',
-          '/phase-0-unit-2/tree/master/week-6/3-playlist',
-          '/phase-0-unit-2/tree/master/week-6/4-bingo-2-solo-challenge',
-          '/phase-0-unit-2/tree/master/week-6/5-inheritance',
-          '/phase-0-unit-2/tree/master/week-6/6-refactoring',
-          '/phase-0-unit-2/blob/master/week-6/7-technical-blog.md',
-          '/phase-0-unit-2/blob/master/week-6/8-cultural-blog.md',
-          '/phase-0-unit-2/tree/master/week-6/9-gps2-2',
-          '/phase-0-unit-2/tree/master/week-6/10-BONUS-challenges'
-        ]
-
-week_7 = ['/phase-0-unit-3/tree/master/week-7/1-code-combat',
-          '/phase-0-unit-3/tree/master/week-7/2-eloquent-js',
-          '/phase-0-unit-3/tree/master/week-7/3-js-variables-objects',
-          '/phase-0-unit-3/tree/master/week-7/4-manipulating-js-objects',
-          '/phase-0-unit-3/tree/master/week-7/5-design-cc-mission-solo-challenge',
-          '/phase-0-unit-3/tree/master/week-7/6-group-project',
-          '/phase-0-unit-3/blob/master/week-7/7-technical-blog.md',
-          '/phase-0-unit-3/blob/master/week-7/8-cultural-blog.md',
-          '/phase-0-unit-3/tree/master/week-7/9-gps3-1',
-          '/phase-0-unit-3/blob/master/week-7/10-accountability-group.md'
-        ]
-
-week_8 = ['/phase-0-unit-3/tree/master/week-8/1-intro-to-databases',
-          '/phase-0-unit-3/tree/master/week-8/2-more-schemas',
-          '/phase-0-unit-3/tree/master/week-8/3-js-gradebook',
-          '/phase-0-unit-3/tree/master/week-8/4-js-tally-votes',
-          '/phase-0-unit-3/blob/master/week-8/5-ruby.md',
-          '/phase-0-unit-3/blob/master/week-8/6-cheatsheet.md',
-          '/phase-0-unit-3/blob/master/week-8/7-technical-blog.md',
-          '/phase-0-unit-3/blob/master/week-8/8-cultural-blog.md',
-          '/phase-0-unit-3/tree/master/week-8/9-gps3-2',
-          '/phase-0-unit-3/blob/master/week-8/10-accountability-group.md',
-          '/phase-0-unit-3/tree/master/phase-0-unit-3/tree/master/week-8/11-BONUS-challenges'
-        ]
-
-week_9 = ['/phase-0-unit-3/blob/master/week-9/1-professional-blog.md',
-          '/phase-0-unit-3/tree/master/week-9/2-JavaScript',
-          '/phase-0-unit-3/blob/master/week-9/3-ruby.md',
-          '/phase-0-unit-3/blob/master/week-9/4-cultural-blog.md',
-          '/phase-0-unit-3/blob/master/week-9/5-cohort-bonding.md',
-          '/phase-0-unit-3/blob/master/week-9/6-accountability-group.md',
-          '/phase-0-unit-3/tree/master/week-9/7-BONUS-challenges'
-        ]
-
-all_weeks = [week_1, week_2, week_3, week_4, week_5, week_6, week_7, week_8, week_9]
-
-desert_rabbits = {Karlsberg: 'aaronkarlsberg',
-                  Esmaili: 'AESM',
-                  Vaughan: 'avaughan89',
-                  Field: 'angelafield',
-                  Oztuncer: 'BanuD',
-                  Vogelgesang: 'kida001',
-                  Giardina: 'cgardens',
-                  Klos: 'eclecticklos',
-                  Kelley: 'danimalkelley',
-                  Cheng: 'gsccheng',
-                  Sison: 'hsison',
-                  McIntyre: 'jeremymcintyre',
-                  Foley: 'johnwfoley',
-                  Luse: 'JLuse',
-                  Won: 'godspeedyoo',
-                  Tice: 'ktice74',
-                  Villwock: 'mtvillwock',
-                  Mears: 'imears',
-                  Chang: 'mikeychang2',
-                  Page: 'senormichaelpage',
-                  Dharamcy: 'www-coder-in',
-                  Salisbury: 'resalisbury',
-                  Fiallo: 'Ridou',
-                  Buren: 'not in socrates',
-                  Idrisov: 'ruslanid',
-                  Chuang: 'victoriachuang',
-                  Fialho: 'vivivf',
-                  Tsang: 'kaawang'
-                }
+require './dbc_data'
 
 puts "Week number:"
 print ">"
@@ -148,12 +6,12 @@ week_number = gets.chomp.to_i
 puts "Challenge number:"
 print ">"
 challenge_num = gets.chomp.to_i
-desert_rabbits.each {|k, v| puts k}
+@desert_rabbits.each {|k, v| puts k}
 puts "\nLast name of student:"
 print ">"
 student = gets.chomp.capitalize.to_sym
 
-while !desert_rabbits.include?(student) # checks for correct name input
+while !@desert_rabbits.include?(student) # checks for correct name input
   puts "\nError: Please enter correct last name:"
   print ">"
   student = gets.chomp.capitalize.to_sym
@@ -161,4 +19,4 @@ end
 
 puts # empty line
 puts "#{student.to_s}'s Week-#{week_number} Challenge-#{challenge_num}:"
-puts "http://github.com/#{desert_rabbits[student]}#{all_weeks[week_number-1][challenge_num-1]}"
+puts "http://github.com/#{@desert_rabbits[student]}#{@all_weeks[week_number-1][challenge_num-1]}"
