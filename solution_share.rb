c30245c0fc24090e21873b0527ae26037e8d6bdc
Maxwell Mears <maxwellmears@gmail.com>
2014-11-26 02:54:01 -0800

    tweeked formating, added comment

--- solution_share.rb
+++ solution_share.rb
@@ -3,20 +3,22 @@ require './dbc_data'
 puts "Week number:"
 print ">"
 week_number = gets.chomp.to_i
+
 puts "Challenge number:"
 print ">"
 challenge_num = gets.chomp.to_i
-@desert_rabbits.each {|k, v| puts k}
+
+@desert_rabbits.each {|k, v| puts k} # prints all last names
 puts "\nLast name of student:"
 print ">"
 student = gets.chomp.capitalize.to_sym
 
-while !@desert_rabbits.include?(student) # checks for correct name input
+while !@desert_rabbits.include?(student) # loop until given a valid last name
   puts "\nError: Please enter correct last name:"
   print ">"
   student = gets.chomp.capitalize.to_sym
 end
 
 puts # empty line
-puts "#{student.to_s}'s Week-#{week_number} Challenge-#{challenge_num}:"
+puts "#{student.to_s}'s Week-#{week_number} Challenge-#{challenge_num} link:"
 puts "http://github.com/#{@desert_rabbits[student]}#{@all_weeks[week_number-1][challenge_num-1]}"
